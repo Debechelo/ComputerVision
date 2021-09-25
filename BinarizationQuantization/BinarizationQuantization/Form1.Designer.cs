501d64787551946391fa7426e7adfd36dc796bca
Debechelo <pashapro1980@gmail.com>
2021-09-25 17:58:39 +0300

    Оцу

--- BinarizationQuantization/BinarizationQuantization/Form1.Designer.cs
+++ BinarizationQuantization/BinarizationQuantization/Form1.Designer.cs
@@ -32,18 +32,25 @@ namespace BinarizationQuantization
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 34);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(646, 490);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -53,25 +60,16 @@ namespace BinarizationQuantization
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(683, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(690, 34);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(646, 490);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 530);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(508, 28);
-            this.comboBox1.TabIndex = 3;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(573, 531);
+            this.numericUpDown1.Location = new System.Drawing.Point(1134, 600);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             8,
             0,
@@ -93,30 +91,97 @@ namespace BinarizationQuantization
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(797, 531);
+            this.button1.Location = new System.Drawing.Point(12, 557);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(234, 112);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Квантование";
+            this.button1.Text = "Квантование по яркости";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 557);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 112);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Оцу Глобальный";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(543, 557);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 112);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Оцу Локальный";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(820, 556);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 112);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Оцу Иерархический";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1341, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(226, 26);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить картинку";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1341, 746);
+            this.ClientSize = new System.Drawing.Size(1341, 680);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -124,9 +189,15 @@ namespace BinarizationQuantization
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- BinarizationQuantization/BinarizationQuantization/Form1.cs
+++ BinarizationQuantization/BinarizationQuantization/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace BinarizationQuantization
 {
@@ -16,44 +17,18 @@ namespace BinarizationQuantization
         {
             InitializeComponent();
         }
+		private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				Bitmap sourceFile = new Bitmap(openFileDialog1.FileName);
+				pictureBox1.Image = sourceFile;
+				pictureBox2.Image = sourceFile;
+			}
+		}
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (comboBox1.Text == "Квантование по яркости")
-            {
-                pictureBox1.BringToFront();
-                pictureBox2.BringToFront();
-            }
-            //else
-            //{
-            //    if (comboBox1.Text == "Бинаризация методом Оцу глобальная")
-            //    {
-            //        pictureBox3.BringToFront();
-            //        pictureBox8.BringToFront();
-            //    }
-            //    else
-            //    {
-            //        if (comboBox1.Text == "Бинаризация методом Оцу локальная")
-            //        {
-            //            pictureBox4.BringToFront();
-            //            pictureBox9.BringToFront();
-            //        }
-            //        else
-            //        {
-            //            if (comboBox1.Text == "Бинаризация методом Оцу иерархическая")
-            //            {
-            //                pictureBox5.BringToFront();
-            //                pictureBox6.BringToFront();
-            //                chart1.BringToFront();
-            //                chart2.BringToFront();
-            //            }
-            //        }
-            //    }
-            //}
-        }
-
-        static Color[] colors = new Color[] { Color.Black, Color.Blue, Color.Green,
-            Color.Red, Color.Yellow, Color.DarkRed, Color.DarkBlue, Color.White };
+        static Color[] colors = new Color[] { Color.Black,  Color.DarkBlue, Color.Blue,
+			Color.Green, Color.DarkRed, Color.Red, Color.Yellow, Color.White };
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -78,7 +53,41 @@ namespace BinarizationQuantization
                 }
             pictureBox2.Image = (Image)image2;
         }
-        private double[] ConvertBrightness(Bitmap bing)
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int[] intensity = new int[256];
+			Bitmap image = new Bitmap(pictureBox1.Image);
+			for (int x = 0; x < image.Width; ++x)
+				for (int y = 0; y < image.Height; ++y)
+				{
+					var t = image.GetPixel(x, y);
+					double c = 0.2126 * t.R + 0.7152 * t.G + 0.0722 * t.B;
+					++intensity[(int)c];	
+				}
+			pictureBox2.Image = GlobalOtsu((image), intensity);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			pictureBox2.Image = LocalOtsu(new Bitmap(pictureBox1.Image));
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			int[] intensity = new int[256];
+			Bitmap image = new Bitmap(pictureBox1.Image);
+			for (int x = 0; x < image.Width; ++x)
+				for (int y = 0; y < image.Height; ++y)
+				{
+					var t = image.GetPixel(x, y);
+					double c = 0.2126 * t.R + 0.7152 * t.G + 0.0722 * t.B;
+					++intensity[(int)c];
+				}
+			pictureBox2.Image = IerarhOtsu(new Bitmap(pictureBox1.Image), intensity, (int)numericUpDown1.Value);
+		}
+
+		private double[] ConvertBrightness(Bitmap bing)
         {
             double[] brightness = new double[1000];
             for (int i = 0; i < bing.Width; ++i)
@@ -86,10 +95,174 @@ namespace BinarizationQuantization
                 for (int j = 0; j < bing.Height; ++j)
                 {
                     var brigh = bing.GetPixel(i, j).GetBrightness();
-                    brightness[(int)(brigh*1000)]++;
+                    brightness[Math.Min((int)(brigh*1000),999)]++;
                 }
             }
             return brightness;
         }
+		static int MethodOtsu(int cntPix, int[] intensity)
+		{
+			double[] disp = new double[intensity.Length];
+			double[] intensityD = new double[intensity.Length];
+			for (int i = 0; i < intensity.Length; ++i)
+				intensityD[i] = 1.0 * intensity[i] / cntPix;
+
+			double q = intensityD[0];
+			double m1 = 0, m2 = 0;
+			double nt = 0, mt = 0;
+			for (int i = 0; i < intensity.Length; ++i)
+			{
+				nt += intensityD[i];
+				mt += intensityD[i] * i;
+			}
+			mt /= nt;
+
+			for (int i = 1; i < intensity.Length; ++i)
+			{
+				m1 = q * m1 + i * intensityD[i];
+				q += intensityD[i];
+				if (q == 0 || q == 1) continue;
+				m1 /= q;
+				m2 = (mt - q * m1) / (1 - q);
+				disp[i] = Math.Sqrt(q * (1 - q) * (m1 - m2) * (m1 - m2));
+			}
+
+			var lst = disp.ToList();
+			var t = lst.FindIndex(x => x == lst.Max());
+			Console.WriteLine("Otsu T: " + t);
+			return t;
+		}
+
+		static void RecMethodOtsu(double[] intensity, List<int> listT, int cntRec)
+		{
+			if (cntRec == 0 || intensity.Length < 3) return;
+			double[] disp = new double[intensity.Length];
+
+
+			double q = intensity[0];
+			double m1 = 0, m2 = 0;
+			double nt = 0, mt = 0;
+			for (int i = 0; i < intensity.Length; ++i)
+			{
+				nt += intensity[i];
+				mt += intensity[i] * i;
+			}
+			mt /= nt;
+
+			for (int i = 1; i < intensity.Length; ++i)
+			{
+				m1 = q * m1 + i * intensity[i];
+				q += intensity[i];
+				if (q == 0 || q == 1) continue;
+				m1 /= q;
+				m2 = (mt - q * m1) / (1 - q);
+				disp[i] = Math.Sqrt(q * (1 - q) * (m1 - m2) * (m1 - m2));
+			}
+
+			var lst = disp.ToList();
+			var t = lst.FindIndex(x => x == lst.Max());
+			listT.Add(t);
+
+			double[] tempInten = new double[t];
+			for (int i = 0; i < t; ++i)
+				tempInten[i] = intensity[i];
+
+			RecMethodOtsu(tempInten, listT, cntRec - 1);
+
+			double[] tempInten1 = new double[intensity.Length - t + 1];
+			for (int i = t; i < intensity.Length; ++i)
+				tempInten1[i - t] = intensity[i];
+			RecMethodOtsu(tempInten1, listT, cntRec - 1);
+		}
+
+		public static Bitmap GlobalOtsu(Bitmap src, int[] intensity)
+		{
+			Bitmap dest = new Bitmap(src.Width, src.Height);
+			Color b = Color.Black, w = Color.White;
+
+			var maxT = MethodOtsu(src.Width * src.Height, intensity);
+			Console.WriteLine();
+			for (int x = 0; x < dest.Width; ++x)
+				for (int y = 0; y < dest.Height; ++y)
+				{
+					var t = src.GetPixel(x, y);
+					if (t.R < maxT)
+						dest.SetPixel(x, y, b);
+					else
+						dest.SetPixel(x, y, w);
+				}
+			return dest;
+		}
+
+		public static Bitmap LocalOtsu(Bitmap src)
+		{
+			Bitmap dest = new Bitmap(src.Width, src.Height);
+			Color b = Color.Black, w = Color.White;
+
+			int[] inten1 = new int[256], inten2 = new int[256];
+			for (int x = 0; x < dest.Width; ++x)
+			{
+				for (int y = 0; y < dest.Height / 2; ++y)
+					++inten1[src.GetPixel(x, y).R];
+				for (int y = dest.Height / 2; y < dest.Height; ++y)
+					++inten2[src.GetPixel(x, y).R];
+			}
+
+			var maxT1 = MethodOtsu(src.Width * src.Height, inten1);
+			var maxT2 = MethodOtsu(src.Width * src.Height, inten2);
+
+			Console.WriteLine();
+
+			for (int x = 0; x < dest.Width; ++x)
+			{
+				for (int y = 0; y < dest.Height / 2; ++y)
+				{
+					var t = src.GetPixel(x, y);
+					if (t.R < maxT1)
+						dest.SetPixel(x, y, b);
+					else
+						dest.SetPixel(x, y, w);
+				}
+				for (int y = dest.Height / 2; y < dest.Height; ++y)
+				{
+					var t = src.GetPixel(x, y);
+					if (t.R < maxT2)
+						dest.SetPixel(x, y, b);
+					else
+						dest.SetPixel(x, y, w);
+				}
+			}
+			return dest;
+		}
+
+		public static Bitmap IerarhOtsu(Bitmap src, int[] intensity, int recCount)
+		{
+			Bitmap dest = new Bitmap(src.Width, src.Height);
+			Color b = Color.Black, w = Color.White;
+
+			List<int> listT = new List<int>();
+
+			var cntPix = src.Width * src.Height;
+			double[] intensityD = new double[256];
+			for (int i = 0; i < intensity.Length; ++i)
+				intensityD[i] = 1.0 * intensity[i] / cntPix;
+
+			RecMethodOtsu(intensityD, listT, recCount);
+			listT = listT.OrderBy(x => x).ToList();
+
+			for (int x = 0; x < dest.Width; ++x)
+				for (int y = 0; y < dest.Height; ++y)
+				{
+					var t = src.GetPixel(x, y);
+
+					var first = listT.FindIndex(c => c > t.R);
+
+					if (first % 2 == 0)
+						dest.SetPixel(x, y, b);
+					else
+						dest.SetPixel(x, y, w);
+				}
+			return dest;
+		}
     }
 }
